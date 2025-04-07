2698985e955a1162ecabf23374159dc3b4df8c13
m_rizkyprtm <m.rizkypratama319@gmail.com>
2025-04-07 23:04:54 +0700

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Instagram, Send } from 'lucide-vue-next';
+</script>
+
+<template>
+    <div id="MainLayout" class="h-screen w-full">
+        <div
+            class="sticky z-30 block w-full border-b border-b-gray-300 bg-white px-3 py-2"
+        >
+            <div class="flex items-center justify-between">
+                <!-- Logo -->
+                <div class="flex items-center">
+                    <Instagram />
+                    <div class="mx-2 h-8 w-px bg-gray-300"></div>
+                    <h1 class="text-xl font-semibold">Instagram</h1>
+                </div>
+
+                <button class="p-2 text-xl">
+                    <Send />
+                </button>
+            </div>
+        </div>
+
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <p class="bg-green-400">Home Page</p>
+    </MainLayout>
+</template>
