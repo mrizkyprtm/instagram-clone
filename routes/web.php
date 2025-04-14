94a9c2097384e9812275cc4c14646c8175c185a5
m_rizkyprtm <m.rizkypratama319@gmail.com>
2025-04-14 12:36:25 +0700

    refactor: need login to access home page

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home/Index');
-})->name('home');
-
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+    Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Home/Index');
+    })->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
